0e7fef419498a3b0f02e14a7a43807a8ae13421f
YuraCoupe <yurik124@gmail.com>
2021-12-08 22:48:44 +0200

    add Task 2

--- src/main/java/ua/goit/javacore4/hw13/jsonplaceholderDemo.java
+++ src/main/java/ua/goit/javacore4/hw13/jsonplaceholderDemo.java
@@ -1,37 +1,56 @@
 package ua.goit.javacore4.hw13;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
 public class jsonplaceholderDemo {
-    private static final String URL = "https://jsonplaceholder.typicode.com/users";
+    private static final String USERS_URL = "https://jsonplaceholder.typicode.com/users";
+    private static final String POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public static void main(String[] args) throws IOException, InterruptedException {
         //create new user to add
         JPHUser defaultUser = createDefaultUser();
         //add new user
-        JPHUser createdUser = jsonplaceholderUtil.createUser(URI.create(URL), defaultUser);
+        JPHUser createdUser = jsonplaceholderUtil.createUser(URI.create(USERS_URL), defaultUser);
         System.out.println("Created user:\n" + createdUser);
         //get user to update some info
-        JPHUser usertoUpdate = jsonplaceholderUtil.getUserById(URI.create(URL), 7);
+        JPHUser usertoUpdate = jsonplaceholderUtil.getUserById(URI.create(USERS_URL), 7);
         //update name
         usertoUpdate.setName("Yurik");
         //update user
-        JPHUser updatedUser = jsonplaceholderUtil.updateUser(URI.create(URL), usertoUpdate);
+        JPHUser updatedUser = jsonplaceholderUtil.updateUser(URI.create(USERS_URL), usertoUpdate);
         System.out.println("Updated user name:\n" + updatedUser.getName());
         System.out.println("Updated user:\n" + updatedUser);
         //delete user
-        jsonplaceholderUtil.deleteUser(URI.create(URL), createdUser.getId());
+        jsonplaceholderUtil.deleteUser(URI.create(USERS_URL), createdUser.getId());
         //get all users
-        List<JPHUser> allUsers = jsonplaceholderUtil.getUsers(URI.create(URL));
+        List<JPHUser> allUsers = jsonplaceholderUtil.getUsers(URI.create(USERS_URL));
         System.out.println("All users:\n" + allUsers);
         //get user by ID
-        JPHUser userById = jsonplaceholderUtil.getUserById(URI.create(URL), 7);
+        JPHUser userById = jsonplaceholderUtil.getUserById(URI.create(USERS_URL), 7);
         System.out.println("User got by ID:\n" + userById);
         //get user by username
-        JPHUser userByUsername = jsonplaceholderUtil.getUserbyUsername(URI.create(URL), "Bret");
+        JPHUser userByUsername = jsonplaceholderUtil.getUserbyUsername(URI.create(USERS_URL), "Bret");
         System.out.println("User got by username:\n" + userByUsername);
+
+        //get user posts
+        int userId = 5;
+        List<Post> userPosts = jsonplaceholderUtil.getUserPosts(URI.create(USERS_URL), userId);
+        System.out.println("User posts: \n" + userPosts);
+        //get user last post id
+        int lastPostId = jsonplaceholderUtil.getUserLastPostId(userPosts);
+        System.out.println("User last post id: " + lastPostId);
+        //get comments to last user post
+        List<Comment> userLastPostComments = jsonplaceholderUtil.getUserLastPostComments(URI.create(POSTS_URL), lastPostId);
+        System.out.println(userLastPostComments);
+        //write comments to file
+        String fileJSON = GSON.toJson(userLastPostComments);
+        jsonplaceholderUtil.fileWriter(userId, lastPostId, fileJSON);
     }
 
     public static JPHUser createDefaultUser() {
--- src/main/java/ua/goit/javacore4/hw13/jsonplaceholderUtil.java
+++ src/main/java/ua/goit/javacore4/hw13/jsonplaceholderUtil.java
@@ -3,11 +3,15 @@ package ua.goit.javacore4.hw13;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Comparator;
 import java.util.List;
 
 public class jsonplaceholderUtil {
@@ -82,5 +86,55 @@ public class jsonplaceholderUtil {
         List<JPHUser> users = GSON.fromJson(response.body(), new TypeToken<List<JPHUser>>() {
         }.getType());
         return users.get(0);
+    }
+
+    public static List<Comment> getUserLastPostComments (URI uri, int postId) throws IOException, InterruptedException {
+        URI newUri = URI.create(String.format("%s/%d/comments", uri.toString(), postId));
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(newUri)
+                .GET()
+                .header("Content-type", "application/json")
+                .build();
+        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        List<Comment> comments = GSON.fromJson(response.body(), new TypeToken<List<Comment>>() {
+        }.getType());
+        return comments;
+    }
+
+    public static List<Post> getUserPosts(URI uri, int userId) throws IOException, InterruptedException {
+        URI newUri = URI.create(String.format("%s/%d/posts", uri.toString(), userId));
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(newUri)
+                .GET()
+                .header("Content-type", "application/json")
+                .build();
+        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        List<Post> posts = GSON.fromJson(response.body(), new TypeToken<List<Post>>() {
+        }.getType());
+        return posts;
+    }
+
+    public static int getUserLastPostId(List<Post> posts) {
+        int maxPostId = posts.stream()
+                .max(Comparator.comparingInt(Post::getId))
+                .get().getId();
+        return maxPostId;
+    }
+
+    public static void fileWriter (int userId, int postId, String string) {
+        File file = new File(String.format("src/main/resources/user-%d-post-%d-comments.json", userId, postId));
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(string);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
